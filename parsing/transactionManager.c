2b830d2ec3f287340bd71d2c02f522884588ddc4
Samurai <samurai@samurai-wtf.localhost>
2014-06-27 20:42:23 -0500

    integra mcrypt con aes-128

--- parsing/transactionManager.c
+++ parsing/transactionManager.c
@@ -7,20 +7,53 @@
 /***********************************************************************
  *  Defines and Includes
  */
-#define DUMP(s, i, buf, sz)  {printf(s);                   \
-                              for (i = 0; i < (sz);i++)    \
-                                  printf("%02x ", buf[i]); \
-                              printf("\n");}
-
 #define _XOPEN_SOURCE
+
+// ------------------------------------------------------------------
+// Signed variables are for wimps  necesarios para SHA-2
+#define uchar unsigned char // 8-bit byte
+#define uint unsigned long  // 32-bit word
+
+// DBL_INT_ADD treats two unsigned ints a and b as one 64-bit integer and adds c to it
+#define DBL_INT_ADD(a,b,c) if (a > 0xffffffff - (c)) ++b; a += c;
+#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
+#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
+
+#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
+#define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
+#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
+
+typedef struct {
+   uchar data[64];
+   uint datalen;
+   uint bitlen[2];
+   uint state[8];
+} SHA256_CTX;
+
+uint k[64] = {
+   0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+   0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+   0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+   0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+   0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+   0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+   0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+   0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+};
+// ----------------------------------------------------------------------
+                              
 #include <ctype.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <assert.h>
 #include <mysql.h>
-#include "aes256.h"
+#include <mcrypt.h>
 
 
 /***********************************************************************
@@ -244,6 +277,150 @@ int isTokenValid (MYSQL *connector,  char *token_id)
 
 } // isTokenValid
 
+/*****************************************************************************
+ * Aplica la transformación SHA-2
+ * 
+ * ctx  - Contexto
+ * data - Datos 
+ */
+void sha256_transform(SHA256_CTX *ctx, uchar data[])
+{  
+   uint a,b,c,d,e,f,g,h,i,j,t1,t2,m[64];
+      
+   for (i=0,j=0; i < 16; ++i, j += 4)
+      m[i] = (data[j] << 24) | (data[j+1] << 16) | (data[j+2] << 8) | (data[j+3]);
+   for ( ; i < 64; ++i)
+      m[i] = SIG1(m[i-2]) + m[i-7] + SIG0(m[i-15]) + m[i-16];
+
+   a = ctx->state[0];
+   b = ctx->state[1];
+   c = ctx->state[2];
+   d = ctx->state[3];
+   e = ctx->state[4];
+   f = ctx->state[5];
+   g = ctx->state[6];
+   h = ctx->state[7];
+   
+   for (i = 0; i < 64; ++i) {
+      t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
+      t2 = EP0(a) + MAJ(a,b,c);
+      h = g;
+      g = f;
+      f = e;
+      e = d + t1;
+      d = c;
+      c = b;
+      b = a;
+      a = t1 + t2;
+   }
+   
+   ctx->state[0] += a;
+   ctx->state[1] += b;
+   ctx->state[2] += c;
+   ctx->state[3] += d;
+   ctx->state[4] += e;
+   ctx->state[5] += f;
+   ctx->state[6] += g;
+   ctx->state[7] += h;
+   
+}  //sha256_transform
+
+/***********************************************************
+ * Inicializa el estado de SHA-2
+ * 
+ * ctx - Contexto
+ */
+void sha256_init(SHA256_CTX *ctx)
+{  
+   ctx->datalen = 0; 
+   ctx->bitlen[0] = 0; 
+   ctx->bitlen[1] = 0; 
+   ctx->state[0] = 0x6a09e667;
+   ctx->state[1] = 0xbb67ae85;
+   ctx->state[2] = 0x3c6ef372;
+   ctx->state[3] = 0xa54ff53a;
+   ctx->state[4] = 0x510e527f;
+   ctx->state[5] = 0x9b05688c;
+   ctx->state[6] = 0x1f83d9ab;
+   ctx->state[7] = 0x5be0cd19;
+   
+}// sha256_init
+
+/**********************************************************
+ * Actualiza el running value del hash
+ * 
+ * ctx  - Contexto
+ * data - datos
+ * len  - Longitud
+ */
+void sha256_update(SHA256_CTX *ctx, uchar data[], uint len)
+{  
+   uint i;
+   
+   for (i=0; i < len; ++i) { 
+      ctx->data[ctx->datalen] = data[i]; 
+      ctx->datalen++; 
+      if (ctx->datalen == 64) { 
+         sha256_transform(ctx,ctx->data);
+         DBL_INT_ADD(ctx->bitlen[0],ctx->bitlen[1],512); 
+         ctx->datalen = 0; 
+      }  
+   }  
+}  //sha256_update 
+
+/*********************************************************
+ * Culmina la generación del SHA-2 hash
+ * 
+ * ctx - Contexto
+ * hash - Hash producido
+ */
+void sha256_final(SHA256_CTX *ctx, uchar hash[])
+{  
+   uint i; 
+   
+   i = ctx->datalen; 
+   
+   // Pad whatever data is left in the buffer. 
+   if (ctx->datalen < 56) { 
+      ctx->data[i++] = 0x80; 
+      while (i < 56) 
+         ctx->data[i++] = 0x00; 
+   }  
+   else { 
+      ctx->data[i++] = 0x80; 
+      while (i < 64) 
+         ctx->data[i++] = 0x00; 
+      sha256_transform(ctx,ctx->data);
+      memset(ctx->data,0,56); 
+   }  
+   
+   // Append to the padding the total message's length in bits and transform. 
+   DBL_INT_ADD(ctx->bitlen[0],ctx->bitlen[1],ctx->datalen * 8);
+   ctx->data[63] = ctx->bitlen[0]; 
+   ctx->data[62] = ctx->bitlen[0] >> 8; 
+   ctx->data[61] = ctx->bitlen[0] >> 16; 
+   ctx->data[60] = ctx->bitlen[0] >> 24; 
+   ctx->data[59] = ctx->bitlen[1]; 
+   ctx->data[58] = ctx->bitlen[1] >> 8; 
+   ctx->data[57] = ctx->bitlen[1] >> 16;  
+   ctx->data[56] = ctx->bitlen[1] >> 24; 
+   sha256_transform(ctx,ctx->data);
+   
+   // Since this implementation uses little endian byte ordering and SHA uses big endian,
+   // reverse all the bytes when copying the final state to the output hash. 
+   for (i=0; i < 4; ++i) { 
+      hash[i]    = (ctx->state[0] >> (24-i*8)) & 0x000000ff; 
+      hash[i+4]  = (ctx->state[1] >> (24-i*8)) & 0x000000ff; 
+      hash[i+8]  = (ctx->state[2] >> (24-i*8)) & 0x000000ff;
+      hash[i+12] = (ctx->state[3] >> (24-i*8)) & 0x000000ff;
+      hash[i+16] = (ctx->state[4] >> (24-i*8)) & 0x000000ff;
+      hash[i+20] = (ctx->state[5] >> (24-i*8)) & 0x000000ff;
+      hash[i+24] = (ctx->state[6] >> (24-i*8)) & 0x000000ff;
+      hash[i+28] = (ctx->state[7] >> (24-i*8)) & 0x000000ff;
+   }  
+}  // sha256_final
+
+
 /**********************************************************************
 Get the AES key based on the user id
 
@@ -251,15 +428,16 @@ connector - Conexion a la base de datos
 usuario   - Codigo de usuario asociado a la llave
 key       - LLave AES a obtener
 */
-int getAESKey( MYSQL *connector, int user_id,  uint8_t *key) {
+int getAESKey( MYSQL *connector, int user_id,  char *key) {
 	
-   char *fixKey = "abcdefghijklmnopqrstuvwxyzabcdef";
+   char *fixKey = "felipe|felipe|1234";
    int i = 0;
 
    for (i=0; i < 31; i++)
-      key[i] = (uint8_t) fixKey[i];
+      key[i] = fixKey[i];
 
    return 1;
+   
    /*
    int ok = 0;
    MYSQL_RES *resultSet;
@@ -454,9 +632,75 @@ char *validate(MYSQL *connector, char** param){
 } // validate
 
 
+/*
+   Cifra un buffer con AES-128 en modo CBC utilizando 
+   la biblioteca MCrypt de Linux 
+    
+   buffer     - Texto a cifrar 
+   buffer_len - Longitud real del buffer 
+   IV         - Vector de inicializacion de CBC 
+   key        - La llave de cifrado 
+   key_len    - Longitud de la llave de cifrado 
+*/
+int encrypt(
+           void* buffer,
+           int buffer_len, /* Because the plaintext could include null bytes*/
+           char* IV,
+           char* key,
+           int key_len
+           )
+{
+   MCRYPT td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+   int blocksize = mcrypt_enc_get_block_size(td);
+   if ( buffer_len % blocksize != 0 )
+   {
+      return 1;
+   }
+
+   mcrypt_generic_init(td, key, key_len, IV);
+   mcrypt_generic(td, buffer, buffer_len);
+   mcrypt_generic_deinit (td);
+   mcrypt_module_close(td);
+   return 0;
+} // encrypt
+
+
+/*
+Descifra un texto cifrado con AES-128 modo CBC utilizando
+la biblioteca MCrypt de Linux 
+    
+   buffer     - Texto a descifrar 
+   buffer_len - Longitud real del buffer 
+   IV         - Vector de inicializacion de CBC 
+   key        - La llave de cifrado 
+   key_len    - Longitud de la llave de cifrado 
+*/
+int decrypt(
+           void* buffer,
+           int buffer_len,
+           char* IV,
+           char* key,
+           int key_len
+           )
+{
+   MCRYPT td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+   int blocksize = mcrypt_enc_get_block_size(td);
+   if ( buffer_len % blocksize != 0 )
+   {
+      return 1;
+   }
+   mcrypt_generic_init(td, key, key_len, IV);
+   mdecrypt_generic(td, buffer, buffer_len);
+   mcrypt_generic_deinit (td);
+   mcrypt_module_close(td);
+   return 0;
+} // decrypt
+
+
 /***********************************************************************
  * Begin method
- */
+ */#include <mcrypt.h>
+
 int main (int argc, char *argv[]) {
 
    char line[81];     // Linea de movimientos
@@ -478,9 +722,13 @@ int main (int argc, char *argv[]) {
    char *password = "Hard+20.";
    char *database = "advlogin";
    
-   aes256_context  ctx; 
-   uint8_t         buf[16];
-   int             user_id = atoi( argv[0]);
+   int    buffer_len; 
+   char   buf[32000];
+   int    user_id = atoi( argv[0]);
+   char   *IV     = "AAAAAAAAAAAAAAAA";
+   char   key[32];
+   int    key_size = 16;
+
 
 
    connector = mysql_init(NULL);
@@ -492,41 +740,42 @@ int main (int argc, char *argv[]) {
    }
 
    // Abra el archivo de transacciones
-   ciphered = fopen("movements.aes", "rt");
+   ciphered = fopen("movements.cif", "r");
    if (ciphered == NULL)
    {
       printf("Archivo inexistente!\n");
       exit(1);
    }
-   
+  
    // Descifre el archivo de movimientos
-   // Obtenga la llave de cifrado con base en el usuario recibido
-    uint8_t key[32];
+   // Obtenga la llave de cifrado con base en el usuario recibido 
+    memset( key, 0, 32 * sizeof(char));
+
     if ( !getAESKey( connector, user_id,  key)) {
 		printf( "No pudo obtener la llave de cifrado");
 		exit(1);
     } 
     
    // Descifre el archivo y guardelo en el archivo "movements.txt"
-   file = fopen("movements.txt", "wt");
+   file = fopen("./movements.txt", "w");
    if (file == NULL)
    {
       printf("No pudo crear archivo de movimientos!\n");
       exit(1);
    }
    
-    while( ! feof( ciphered)) {
-		fread( buf, sizeof( uint8_t), 16, ciphered);
-        aes256_init(&ctx, key);
-        aes256_decrypt_ecb(&ctx, buf);
-        fwrite( buf, sizeof(char), 16, file);
+    printf("::: Voy a descifrar");
+    if ( ! feof( ciphered)) {
+		buffer_len = fread( buf, sizeof(char), 32000, ciphered);
+		printf("::: length= %d", buffer_len);
+        decrypt(buf, buffer_len, IV, key, key_size);
+        fwrite( buf, sizeof(char), buffer_len, file);
     }
-    aes256_done(&ctx);
     fclose(ciphered);
     fclose(file);
     
    // Procese el archivo descifrado
-   file = fopen("movements.txt", "rt");
+   file = fopen("movements.txt", "r");
    if (file == NULL)
    {
       printf("Archivo claro de movimientos inexistente!\n");
